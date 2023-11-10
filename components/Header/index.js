e9b468a3140eaf838695f245c4fab10035f39bd2
Amitesh Tripathi <1amiteshtripathi@gmail.com>
2023-11-10 18:35:01 -0500

    Second Corrections

--- components/Header/index.js
+++ components/Header/index.js
@@ -77,7 +77,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   {showResume && (
                     <Button
                       onClick={() =>
-                        window.open("mailto:hello@chetanverma.com")
+                        window.open("mailto:theamiteshtripathi@gmail.com")
                       }
                     >
                       Resume
@@ -85,7 +85,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:theamiteshtripathi@gmail.com")}
                   >
                     Contact
                   </Button>
